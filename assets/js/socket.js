cf3ecefd5ecae810c22786046a5dc9970ca50de4
Edier Andres Villaneda Navarro <ediervillaneda@gmail.com>
2020-06-05 08:23:40 -0500

    Eliminación de comentarios

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,43 +1,34 @@
 function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
     results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
-var prodId = getParameterByName("aforo");
 //alert("Hay un aumento en el aforo de: "+prodId+ " personas");
 
 var socket;
 
 function init() {
-	// Apuntar a la IP/Puerto configurado en el contructor del WebServerSocket, que es donde está escuchando el socket.
-	var host = "ws://localhost:9000";
-	try {
-		socket = new WebSocket(host);
-		/*log('WebSocket - status jk '+socket.readyState);*/
-		/*socket.onopen    = function(msg) {
-							   log("Welcome - status "+this.readyState);
-						   };*/
-		socket.onmessage = function(msg)
-		{
-		    Push.create("Aforo Supera "+msg.data+" personas", { //Titulo de la notificación
-			body: "Presenta un Aforo de "+msg.data+" personas", //Texto del cuerpo de la notificación
-			icon: 'img/alert.png', //Icono de la notificación
-			timeout: 10000, //Tiempo de duración de la notificación
-			onClick: function () {//Función que se cumple al realizar clic cobre la notificación
-				window.location = "http://localhost/intersoftware/alertas.html?aforo="+msg.data; //Redirige a la siguiente web
-				this.close(); //Cierra la notificación
-			}
-		});
-
-		};
-		/*socket.onclose   = function(msg) {
-							   log("Disconnected - status "+this.readyState);
-						   };*/
-	}
-	catch(ex){
-		log(ex);
-	}
-	$("msg").focus();
-	setTimeout(init,1000);
+  // Apuntar a la IP/Puerto configurado en el contructor del WebServerSocket, que es donde está escuchando el socket.
+  var host = "ws://localhost:9000";
+  try {
+    socket = new WebSocket(host);
+    socket.onmessage = function (msg) {
+      Push.create("Aforo Supera " + msg.data + " personas", {
+        //Titulo de la notificación
+        body: "Presenta un Aforo de " + msg.data + " personas", //Texto del cuerpo de la notificación
+        icon: "img/alert.png", //Icono de la notificación
+        timeout: 10000, //Tiempo de duración de la notificación
+        onClick: function () {
+          //Función que se cumple al realizar clic cobre la notificación
+          window.location = "http://localhost/intersoftware/alertas.html?aforo=" + msg.data; //Redirige a la siguiente web
+          this.close(); //Cierra la notificación
+        },
+      });
+    };
+  } catch (ex) {
+    log(ex);
+  }
+  $("msg").focus();
+  setTimeout(init, 1000);
 }
