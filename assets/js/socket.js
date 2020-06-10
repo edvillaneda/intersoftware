64757291575c6b5c28cb783e2e11e30be4279996
Edier Andrés Villaneda Navarro <ediervillaneda@gmail.com>
2020-06-10 13:21:00 -0500

    cambios menores

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -7,7 +7,8 @@ function getParameterByName(name) {
     : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-var prodId = getParameterByName("aforo");
+var valueAforo = getParameterByName("aforo");
+var valueTemp = getParameterByName("temperatura");
 
 //alert("Hay un aumento en el aforo de: "+prodId+ " personas");
 
@@ -19,16 +20,18 @@ function init() {
   try {
     socket = new WebSocket(host);
     socket.onmessage = function (msg) {
-
       var obj = JSON.parse(msg.data);
+      var parms = "";
 
       switch (obj.titulo) {
         case "aforo":
           message = "Aforo Supera " + obj.valor + " personas";
+          parms = "aforo=" + obj.valor + "&temperatura=0";
           break;
         case "temperatura":
           message =
             "Hemos detectado una temperatura mayor a " + obj.valor + "ºC";
+          parms = "aforo=0&temperatura=" + obj.valor;
           break;
       }
 
@@ -40,7 +43,7 @@ function init() {
         onClick: function () {
           //Función que se cumple al realizar clic cobre la notificación
           window.location =
-            "http://localhost/intersoftware/alertas.html?aforo=" + obj.valor; //Redirige a la siguiente web
+            "http://localhost/intersoftware/alertas.html?" + parms; //Redirige a la siguiente web
           this.close(); //Cierra la notificación
         },
       });
